ff587952d663cf6793cc4d458fb25b1e21dd83bd
Maria <maria.destephanis57@gmail.com>
2025-01-22 15:16:37 +0100

    Affichage conditionnel

---
+++ .l/exercices.js
@@ -0,0 +1,7 @@
+const nom_boutique = "SuperBoutique"
+let number_potion_soin = 100
+let prix_potion_soin = 10
+let boutique_open = false
+
+console('Bienvenue dans la boutique <nom_boutique> Aventurier ! 🎉') = true
+console('La boutique <nom_boutique> est fermée, revenez plus tard Aventurier ! 😴') = false
