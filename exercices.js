0e594aab3a8e01748e5b218976a21aecb6d9f69e
Maria <maria.destephanis57@gmail.com>
2025-01-22 16:12:55 +0100

    Calcul du prix total d'une commande de potion 🪙

--- exercices.js
+++ exercices.js
@@ -1,13 +1,21 @@
+// Défintion de variable 
 const nom_boutique = "L'Arcane Emporium";
 let number_potion_soin = 100;
 let prix_potion_soin = 10;
 let nom_sorcier = "Akana"; // Nom du sorcier
 
 console.log("Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔");
+// Calcul du prix total d'une commande de potion 🪙
+let quantite_potion = parseInt(prompt("Combien de potions de soin souhaitez-vous acheter ?"));
+let prix_total = quantite_potion * prix_potion_soin;
+console.log(`Prix de ${quantite_potion} potions de soins : ${prix_total} 🪙 mon cher Aventurier. 💸`);
+
 console.log("1. Le nom de la boutique");
 console.log("2. Le nom du Sorcier");
 console.log("3. Le prix d'une potion de soin");
 console.log("4. La quantité d'une potion de soin");
+
+//Affichage conditionnel avec switch
 
 let choix = parseInt(prompt("Fais ton choix entre 1 et 4 :"));
 
