953c8dc20b90e8ff18228f6361a64faa91142f32
Maria <maria.destephanis57@gmail.com>
2025-01-22 15:56:29 +0100

    Affichage conditionnel avec switch

--- .l/exercices.js
+++ .l/exercices.js
@@ -1,7 +1,11 @@
-const nom_boutique = "SuperBoutique"
+// Défintion de variable 
+const nom_boutique = "L'Arcane Emporium"
 let number_potion_soin = 100
 let prix_potion_soin = 10
 let boutique_open = false
+//Affichage conditionnel
+console.log('Bienvenue dans la boutique <nom_boutique> Aventurier ! 🎉') = true
+console.log('La boutique <nom_boutique> est fermée, revenez plus tard Aventurier ! 😴') = false
 
-console('Bienvenue dans la boutique <nom_boutique> Aventurier ! 🎉') = true
-console('La boutique <nom_boutique> est fermée, revenez plus tard Aventurier ! 😴') = false
+//Affichage conditionnel avec switch
+console.log('Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔')
--- exercices.js
+++ exercices.js
@@ -1,4 +1,29 @@
-const nom_boutique = "SuperBoutique"
-let number_potion_soin = 100
-let prix_potion_soin = 10
-let boutique_open = false
+const nom_boutique = "L'Arcane Emporium";
+let number_potion_soin = 100;
+let prix_potion_soin = 10;
+let nom_sorcier = "Akana"; // Nom du sorcier
+
+console.log("Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔");
+console.log("1. Le nom de la boutique");
+console.log("2. Le nom du Sorcier");
+console.log("3. Le prix d'une potion de soin");
+console.log("4. La quantité d'une potion de soin");
+
+let choix = parseInt(prompt("Fais ton choix entre 1 et 4 :"));
+
+switch (choix) {
+    case 1:
+        console.log(`Le nom de la boutique est : ${nom_boutique}`);
+        break;
+    case 2:
+        console.log(`Le nom du Sorcier est : ${nom_sorcier}`);
+        break;
+    case 3:
+        console.log(`Le prix d'une potion de soin est : ${prix_potion_soin} pièces d'or`);
+        break;
+    case 4:
+        console.log(`La quantité d'une potion de soin est : ${number_potion_soin}`);
+        break;
+    default:
+        console.log("Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix ! 😕");
+}
